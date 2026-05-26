d6304c86b232ad8d0339828ebe23aefb98537556
seannxh <nohsean6@gmail.com>
2026-05-26 17:52:17 -0500

    Review Manual frontend controller updated

---
+++ code-review/src/main/java/com/sean/code_review/config/ReviewController.java
@@ -0,0 +1,53 @@
+package com.sean.code_review.config;
+
+import com.sean.code_review.model.entity.Review;
+import com.sean.code_review.repository.ReviewRepository;
+import com.sean.code_review.service.ClaudeReviewService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reviews")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ReviewController {
+
+    private final ReviewRepository reviewRepository;
+    private final ClaudeReviewService claudeReviewService;
+
+    public ReviewController(ReviewRepository reviewRepository,
+                            ClaudeReviewService claudeReviewService) {
+        this.reviewRepository = reviewRepository;
+        this.claudeReviewService = claudeReviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Review> startManualReview(@RequestBody ReviewRequest request) {
+        String repoFullName = request.getRepoUrl()
+                .replace("https://github.com/", "")
+                .replace("http://github.com/", "")
+                .replace(".git", "")
+                .trim();
+
+        Review review = new Review();
+        review.setRepoFullName(repoFullName);
+        review.setPrNumber(request.getPrNumber());
+        review.setPrTitle("Manual Review");
+        review.setStatus("pending");
+
+        Review savedReview = reviewRepository.save(review);
+
+        new Thread(() -> claudeReviewService.reviewPullRequest(savedReview)).start();
+
+        return ResponseEntity.ok(savedReview);
+    }
+
+    @Setter
+    @Getter
+    public static class ReviewRequest {
+        private String repoUrl;
+        private int prNumber;
+
+    }
+}
